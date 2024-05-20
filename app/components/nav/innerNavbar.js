dd8fdf61faa8b060e20cd9fbd4b3de789e71e120
dbryan17 <dfbryan17@gmail.com>
2024-05-20 14:16:01 -0600

    navbar done enough

--- app/components/nav/innerNavbar.js
+++ app/components/nav/innerNavbar.js
@@ -1,42 +1,87 @@
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 export default function InnerNavBar() {
+  const pathname = usePathname();
+  console.log(pathname);
+
+  const pathHas = (str) => {
+    return pathname.includes(str);
+  };
+
   return (
-    <div className="navbar-end">
-      <Link href="/about" className="navbar-item has-background-primary">
+    <>
+      <Link
+        href="/about"
+        className={
+          "navbar-item has-background-primary" +
+          (pathHas("about") ? " navbar-item-active" : "")
+        }
+      >
         About
       </Link>
-      <Link href="/projects" className="navbar-item has-background-primary">
+      <Link
+        href="/projects"
+        className={
+          "navbar-item has-background-primary" +
+          (pathHas("projects") ? " navbar-item-active" : "")
+        }
+      >
         Projects
       </Link>
-      <div className="navbar-item has-dropdown is-hoverable">
-        <Link className="navbar-link has-background-primary" href="/blogs">
+      <div className="navbar-item has-dropdown is-hoverable has-background-primary">
+        <Link
+          className={
+            "navbar-link has-background-primary is-arrowless" +
+            (pathHas("blogs") ? " navbar-item-active" : "")
+          }
+          href="/blogs"
+        >
           Blogs
         </Link>
         <div className="navbar-dropdown has-background-primary">
           <Link
             href="/blogs/henry"
-            className="navbar-item has-background-primary"
+            className={
+              "navbar-item has-background-primary" +
+              (pathHas("henry") ? " navbar-item-active" : "")
+            }
           >
             Henry's Blogs
           </Link>
         </div>
       </div>
-      <div className="navbar-item has-dropdown is-hoverable">
-        <Link className="navbar-link has-background-primary" href="/tools">
+      <div className="navbar-item has-dropdown is-hoverable has-background-primary">
+        <Link
+          className={
+            "navbar-link has-background-primary is-arrowless" +
+            (pathHas("tools") ? " navbar-item-active" : "")
+          }
+          href="/tools"
+        >
           Small Tools
         </Link>
         <div className="navbar-dropdown has-background-primary">
           <Link
             href="/tools/counter"
-            className="navbar-item has-background-primary"
+            className={
+              "navbar-item has-background-primary" +
+              (pathHas("counter") ? " navbar-item-active" : "")
+            }
           >
             Counter
           </Link>
         </div>
       </div>
-      <Link href="/resume" className="navbar-item has-background-primary">
+      <Link
+        href="/resume"
+        className={
+          "navbar-item has-background-primary" +
+          (pathHas("resume") ? " navbar-item-active" : "")
+        }
+      >
         Resume
       </Link>
-    </div>
+    </>
   );
 }
--- app/components/nav/navbar.js
+++ app/components/nav/navbar.js
@@ -10,28 +10,36 @@ TODO
 */
 export default function Navbar() {
   const [burgerIsActive, setBurgerIsActive] = useState(false);
+
   console.log(burgerIsActive);
   return (
     <nav className="navbar has-background-primary">
-      <Link
-        href="/"
-        className="navbar-item navbar-brand has-background-primary"
-      >
-        Dakota Bryan
-      </Link>
-      {/* todo fix this */}
-      <div
-        className={"navbar-burger" + (burgerIsActive ? " is-active" : "")}
-        data-target={burgerIsActive ? "navbar-content" : ""}
-        onClick={() => setBurgerIsActive((state) => !state)}
-      >
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
+      <div className="navbar-brand">
+        <Link href="/" className="navbar-item has-background-primary">
+          Dakota Bryan
+        </Link>
+        <div
+          className={"navbar-burger" + (burgerIsActive ? " is-active" : "")}
+          onClick={() => setBurgerIsActive((state) => !state)}
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </div>
       </div>
-      <div className="navbar-menu">
-        <InnerNavBar />
+
+      <div
+        className={
+          "has-background-primary navbar-menu" +
+          (burgerIsActive ? " is-active" : "")
+        }
+        id="navContent"
+      >
+        <div className="navbar-start"></div>
+        <div className="navbar-end has-background-primary">
+          <InnerNavBar />
+        </div>
       </div>
     </nav>
   );
