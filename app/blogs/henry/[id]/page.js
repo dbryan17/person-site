52f2c266a5dd5901d8a076a23cb2f01f5b78f800
dbryan17 <dfbryan17@gmail.com>
2024-05-27 22:48:36 -0600

    henry blog zero up

--- app/blogs/henry/[id]/page.js
+++ app/blogs/henry/[id]/page.js
@@ -1,5 +1,21 @@
 import { blogs } from "@/app/data/blogs/henryBlogs";
+import { parse } from "@/app/utils/parser/parser";
 
 export default function BlogPage({ params }) {
-  return <div>{blogs[params.id].title}</div>;
+  const blog = blogs[params.id];
+
+  // title, author, date, content
+  return (
+    <div className="slim-container">
+      <section className="page-chunk">
+        <section className="hero">
+          <div className="hero-body">
+            <p className="title has-text-success is-size-2">{blog.title}</p>
+            <p className="subtitle">{blog.date}</p>
+          </div>
+        </section>
+        <div>{parse(blog.content)}</div>
+      </section>
+    </div>
+  );
 }
