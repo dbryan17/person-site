ac1bcd6b5c7ae09e2595d855ce7a10da2758412b
dbryan17 <dfbryan17@gmail.com>
2024-06-03 13:43:21 -0600

    add cw

--- app/blogs/page.js
+++ app/blogs/page.js
@@ -1,3 +1,12 @@
 export default function BlogsHome() {
-  return <div>Here</div>;
+  return (
+    <iframe
+      style={{ height: "90vh", width: "100%" }}
+      src="https://crosshare.org/embed/H0lO84N26BgKdC1i2qnk/RE40STQaHPd2pcw9Dsbkbzk9dpn2"
+      frameborder="0"
+      allowfullscreen="true"
+      allowtransparency="true"
+      allow="clipboard-write *"
+    ></iframe>
+  );
 }
